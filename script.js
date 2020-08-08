b2fc48d8c95b83ddce7b4ec81153e542e814fb64
Alex Blake <alexblakela@gmail.com>
2020-08-08 10:59:03 -0700

    AB - Initial Push of test API script

---
+++ script.js
@@ -0,0 +1,35 @@
+/* UCLA PROJECT 01 */
+/* FiveStarRecipes */
+
+/* Credentials */
+var appId = "5cf2449c";
+var appKey = "5cf1678b66b687897855b342ee20ede8";
+var appUrl = "https://api.edamam.com/search?";
+var appQuery = "chicken";
+
+/* API CALL */
+function recipePull (parameters) {
+	var appCall = appUrl + "app_id=" + appId + "&app_key=" + appKey + "&q=" + appQuery;
+	$.get(appCall, function( result ) {
+		/* Pull values */
+		
+
+		console.log("pulled");
+	}).done(function() {
+		/* Update page with results */
+		console.log("updated");
+	});
+}
+
+/* Event Listener */
+$(document).ready(function() {
+	$("#search").click(function(event) {
+		event.preventDefault();
+		console.log("search started");
+
+		/* Run API Call */
+		recipePull();
+
+
+	});
+});
