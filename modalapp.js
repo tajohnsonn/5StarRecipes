c57c8e4f0c3f29202cb700d1c178457a10e6c9c0
Bluebird <stephany1445@hotmail.com>
2020-08-13 18:29:10 -0700

    Created the last function so it allows user to exit out of the modal by clicking the modal bg Created an img element in the html

--- modalapp.js
+++ modalapp.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   var closeModalBtn = $(".modal-close");
 
   button.on("click", function () {
-    console.log("button");
+    console.log("Open");
 
     contentModal.css({ display: "block" });
   });
@@ -16,11 +16,13 @@ $(document).ready(function () {
     contentModal.css({ display: "none" });
   });
 
-  $(window).on("click", function (event) {
-    console.log(event);
+  $(window).click(function (event) {
+    var tar = $(event.target).attr("class");
+    // var tar = $(event[(target = "div.modal-background")]);
 
-    // if ((EventTarget.is = $("div.modal-background"))) {
-    //   contentModal.css({ display: "none" });
-    // }
+    if (tar == "modal-background") {
+      // console.log("Hi Im BG");
+      contentModal.css({ display: "none" });
+    }
   });
 });
